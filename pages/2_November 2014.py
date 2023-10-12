c318cf47da97d9f67daf98e2b78111f6703e0d3c
jeandtx <jean.doutriaux@gmail.com>
2023-10-12 23:13:07 +0200

    added november focus

--- pages/2_November 2014.py
+++ pages/2_November 2014.py
@@ -7,15 +7,34 @@ carbone = load_clean_data()
 """
 ### What happened in in November 2014 !? 🤔
 """
+with st.echo(code_location='top'):
+    nov2014 = carbone[carbone['Date de modification'] == 'Novembre 2014']
+histfunc = st.radio("Select a histogram function",
+                    ('count', 'sum', 'avg', 'min', 'max'))
+log = st.checkbox("Logarithmic scale")
 
-# drop_x = st.selectbox("X-axis", ['Code de la catégorie', 'Nom attribut français', 'Nom base français', 'Sous-localisation géographique français'], key=3)
+with st.echo(code_location='top'):
+    fig = px.histogram(nov2014, x='Code de la catégorie', y='Total poste non décomposé',
+                       title='Biggest polluters in the industry', histfunc=histfunc)
+    fig.update_layout(showlegend=False)
+    fig.update_xaxes(categoryorder='total descending')
+    if log:
+        fig.update_layout(yaxis_type="log")
+    st.plotly_chart(fig)
 
-# # Generate the histogram plot
-# with st.echo(code_location='top'):
-#     nov2014 = carbone[carbone['Date de modification'] == 'Novembre 2014']
 
-#     fig = px.pie(nov2014, values='Total poste non décomposé', names=drop_x, title='Focus on November 2014')
-#     fig.update_traces(textposition='inside', textinfo='none')
-#     fig.update_layout(showlegend=False) 
+fig = px.histogram(nov2014[nov2014['Total poste non décomposé'] > 100], x='Nom attribut français',
+                   y='Total poste non décomposé', title='Biggest polluters (Company name) in the industry', histfunc=histfunc)
+fig.update_layout(showlegend=False)
+fig.update_xaxes(categoryorder='total descending')
+if log:
+    fig.update_layout(yaxis_type="log")
+st.plotly_chart(fig)
 
-#     st.plotly_chart(fig)
+fig = px.histogram(nov2014[nov2014['Total poste non décomposé'] > 100], x='Nom base français',
+                   y='Total poste non décomposé', title='Biggest polluters (Object name) in the industry', histfunc=histfunc)
+fig.update_layout(showlegend=False)
+fig.update_xaxes(categoryorder='total descending')
+if log:
+    fig.update_layout(yaxis_type="log")
+st.plotly_chart(fig)
